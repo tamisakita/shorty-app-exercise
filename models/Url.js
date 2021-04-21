5ba44958d1367e898c7575c802e3fd5a736bd5b3
Tami Pipoca <tamisakita@hotmail.com>
2021-04-21 17:38:27 -0300

    add seeds, created table and listed urls

--- models/Url.js
+++ models/Url.js
@@ -6,6 +6,7 @@ const urlSchema = new Schema({
   redirectUrl: { type: String, required: true },
   shortUrl: { type: String, required: true },
   expirationDateMs: { type: Number, required: true },
+  owner: { type: Schema.Types.ObjectId, ref: 'user', required: true },
 },
 {
   timestamps: true,
